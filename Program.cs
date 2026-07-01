3df1f91cc96e8b19f545cab2dfae4658218bd37f
Eshraq <eshraq416@gmail.com>
2026-07-01 21:13:43 +0400

    task2

--- Program.cs
+++ Program.cs
@@ -16,9 +16,19 @@
             Console.WriteLine("Height: " + height);
             Console.WriteLine("Student: " + isStudent);
 
+            //////////////////////////////////////////////////////////////////
+            //task2
+
+            Console.Write("Enter the length of the rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+            double area = length * width;
+            double perimeter = 2 * (length + width);
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
 
 
-    
         }
     }
 }
